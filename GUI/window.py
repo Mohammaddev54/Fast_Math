20139f9d53d47b05e78c86364bb326d779bd54f3
Mohammad_dev <mohammaddev54@gmail.com>
2025-11-26 19:42:14 -0600

    Added buttons & some widgets

--- GUI/window.py
+++ GUI/window.py
@@ -30,6 +30,8 @@ class MyWindow():
         top_frame.grid(row=0, column=0, columnspan=3, sticky="ewns")
 
         top_frame.grid_columnconfigure(0, weight=1)
+        top_frame.grid_rowconfigure(0, weight=1)
+        top_frame.grid_columnconfigure(2, weight=1)
         top_frame.grid_rowconfigure(1, weight=1)
 
         middle_frame = Frame(root_frame, bg="#FFFF00")
@@ -38,25 +40,30 @@ class MyWindow():
         bottom_frame = Frame(root_frame, bg="#FFFFFF")
         bottom_frame.grid(row=2, column=0, columnspan=3, sticky="ewns")
 
-        timer_frame = Frame(middle_frame)
-        timer_frame.grid(row=0, column=0)
-
-        timer_buttons_frame = Frame(middle_frame)
-        timer_buttons_frame.grid(row=0, column=1)
-
         # Labels
         math_expression_question_label = Label(top_frame, text="QUESTION!")
         math_expression_question_label.grid(row=0, column=0, columnspan=3)
 
         math_expression = Label(top_frame, text="a^2 + b^2 = c^2")
-        math_expression.grid(row=1, column=0, columnspan=2)
+        math_expression.grid(row=1, column=0)
 
+        timer_animation = Label(middle_frame, width=10, height=5, bg="#000000")
+        timer_animation.grid(row=0, column=0)
         
 
         # Entries
         user_answer_entry = Entry(top_frame)
-        user_answer_entry.grid(row=1, column=3, columnspan=2)
+        user_answer_entry.grid(row=1, column=2, columnspan=2)
 
+        # Buttons
+        start_button = Button(middle_frame, text="Start")
+        start_button.grid(row=1, column=0)
+
+        previous_button = Button(bottom_frame, text="Previous")
+        previous_button.grid(row=0, column=0)
+
+        next_button = Button(bottom_frame, text="Next")
+        next_button.grid(row=0, column=3)
 
 
 
